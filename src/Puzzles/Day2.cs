2b6deed380b01eb0beaaf7513507a66f9f46e8d6
Dmitry Maltsev <dmitry.maltsev@dataart.com>
2021-12-02 11:11:19 +0300

    Day 2. Generalized solution.

--- src/Puzzles/Day2.cs
+++ src/Puzzles/Day2.cs
@@ -4,19 +4,29 @@ public class Day2
 {
     public int Puzzle1()
     {
-        var lines = Routines.ReadInputLines(nameof(Day2));
+        var (position, depth, _) = Solution(GetInstructions());        
+        return position * depth;
+    }
 
+    public int Puzzle2()
+    {
+        var (position, _, aim) = Solution(GetInstructions());        
+        return position * aim;
+    }
+
+    (int position, int depth, int aim) Solution(IEnumerable<(string move, int step)> instructions)
+    {
+        var aim = 0;
         var depth = 0;
         var position = 0;
         
-        foreach (var line in lines)
+        foreach (var (move, step) in instructions)
         {
-            var (move, step) = ParseInstruction(line);
-            
             switch (move)
             {
                 case "forward":
                     position += step;
+                    aim += depth * step;
                     break;
                 case "down":
                     depth += step;
@@ -26,43 +36,18 @@ public class Day2
                     break;
             }
         }
-        
-        return depth * position;
-    }
 
-    public int Puzzle2()
-    {
-        var lines = Routines.ReadInputLines(nameof(Day2));
-
-        var aim = 0;
-        var depth = 0;
-        var position = 0;
-        
-        foreach (var line in lines)
-        {
-            var (move, step) = ParseInstruction(line);
-            
-            switch (move)
-            {
-                case "forward":
-                    position += step;
-                    depth += aim * step;
-                    break;
-                case "down":
-                    aim += step;
-                    break;
-                case "up":
-                    aim -= step;
-                    break;
-            }
-        }
-        
-        return depth * position;
+        return (position, depth, aim);
     }
 
     (string move, int step) ParseInstruction(string line)
     {
         var parts = line.Split(' ');
         return (move: parts[0], step: int.Parse(parts[1]));
+    }
+
+    IEnumerable<(string move, int step)> GetInstructions()
+    {
+        return Routines.ReadInputLines(nameof(Day2)).Select(ParseInstruction);
     }
 }
