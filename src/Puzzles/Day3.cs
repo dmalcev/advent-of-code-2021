19960431ce12e89c1f0f4b279b16ea71efb2bdab
Dmitry Maltsev <dmitry.maltsev@dataart.com>
2021-12-03 16:36:00 +0300

    Day 3. Puzzle 1. Alternative.

--- src/Puzzles/Day3.cs
+++ src/Puzzles/Day3.cs
@@ -4,40 +4,6 @@ public class Day3
 {
     public int Puzzle1()
     {
-        var matrix = GetMatrix();
-        
-        var width = matrix[0].Length;
-        var half = matrix.Length / 2;
-        
-        var gammaBin = new char[width];
-        var epsilonBin = new char[width];
-
-        var count = 0;
-        
-        for (var col = 0; col < width; col++)
-        {
-            foreach (var row in matrix)
-            {
-                if (row[col] == '1')
-                    count++;
-                
-                if (count > half)
-                    break;
-            }
-
-            gammaBin[col] = count > half ? '1' : '0';
-            epsilonBin[col] = count > half ? '0' : '1';
-            count = 0;
-        }
-
-        var gammaInt = ToInt(new string(gammaBin));
-        var epsilonInt = ToInt(new string(epsilonBin));
-
-        return gammaInt * epsilonInt;
-    }
-
-    public int Puzzle1Lines()
-    {
         var lines = Routines.ReadInputLines(nameof(Day3));
 
         var count = 0;
@@ -59,21 +25,30 @@ public class Day3
         var half = count / 2;
         
         var gammaBit = result.Select(x => x > half ? '1' : '0').ToArray();
-        var gamma = ToInt(new string(gammaBit));
-        
         var epsilonBit = result.Select(x => x > half ? '0' : '1').ToArray();
+        
+        var gamma = ToInt(new string(gammaBit));
         var epsilon = ToInt(new string(epsilonBit));
 
         return gamma * epsilon;
     }
 
-    char[][] GetMatrix()
+    public int Puzzle2()
     {
-        return Routines.ReadInputLines(nameof(Day3)).Select(x => x.ToCharArray()).ToArray();
+        var lines = Routines.ReadInputLines(nameof(Day3)).ToList();
+
+        
     }
 
-    static int ToInt(string binary)
+    int ToInt(string binary)
     {
         return Convert.ToInt32(binary, 2);
+    }
+
+    bool IsMostCommon(string line, char bit)
+    {
+        var half = line.Length / 2;
+        var count = line.Count(c => c == bit);
+        return count > half;
     }
 }
