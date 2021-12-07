100651f6dee219258d99fa94267c155364042137
Dmitry Maltsev <dmitry.maltsev@dataart.com>
2021-12-07 16:13:40 +0300

    Day 7. Simplified solution.

--- src/Puzzles/Day7.cs
+++ src/Puzzles/Day7.cs
@@ -4,49 +4,27 @@ public class Day7
 {
     public long Puzzle1()
     {
-        var inputs = Routines.ReadInput(nameof(Day7)).Split(",").Select(int.Parse);
+        var inputs = Routines.ReadInput(nameof(Day7)).Split(",").Select(int.Parse).ToList();
         return FindMinFuel(inputs, false);
     }
 
     public long Puzzle2()
     {
-        var inputs = Routines.ReadInput(nameof(Day7)).Split(",").Select(int.Parse);
+        var inputs = Routines.ReadInput(nameof(Day7)).Split(",").Select(int.Parse).ToList();
         return FindMinFuel(inputs, true);
     }
 
-    int FindMinFuel(IEnumerable<int> inputs, bool stepsGrow)
+    int FindMinFuel(List<int> positions, bool stepsGrow)
     {
-        var groups = inputs
-            .GroupBy(x => x)
-            .Select(g => new Group(g.Key, g.Count()))
-            .ToList();
+        var fuels =
+            from target in Enumerable.Range(1, positions.Max() - positions.Min() + 1)
+            let posFuel =
+                from pos in positions
+                let distance = Math.Abs(pos - target)
+                let fuel = stepsGrow ? distance * (distance + 1) / 2 : distance
+                select fuel
+            select posFuel.Sum();
 
-        var start = groups.Min(x => x.Key);
-        var end = groups.Max(x => x.Key);
-
-        var minFuel = -1;
-        
-        for (var i = start; i <= end; i++)
-        {
-            var fuel = 0;
-            
-            foreach (var (source, count) in groups)
-            {
-                var distance = Math.Abs(source - i);
-                var increment = stepsGrow
-                    ? distance * (distance + 1) / 2
-                    : distance;
-                
-                fuel += increment * count;
-                
-                if (minFuel != -1 && fuel > minFuel) break;
-            }
-
-            if (minFuel == -1 || fuel < minFuel) minFuel = fuel;
-        }
-
-        return minFuel;
+        return fuels.Min();
     }
-
-    record Group(int Key, int Count);
 }
